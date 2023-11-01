463f894e41ba3fcd49d47079078f239cd7e4a4c6
Sayed Afnan Khazi <83779299+Sayed-Afnan-Khazi@users.noreply.github.com>
2023-11-01 16:00:08 +0530

    Added the redux store and provider

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import './index.css';
 import App from './containers/App'
 import 'tachyons';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import { searchRobots } from './reducers';
+
+const store = createStore(searchRobots); // The root reducer is used here
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}> 
+    {/* The provider takes care of passing the store down to every single element so that we don't have to do so as props */}
+      <App/>
+    </Provider>
   </React.StrictMode>
 );
 
